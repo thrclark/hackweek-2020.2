aa2fbc3a874c5e37a042a52fb2e77a0537c26cb3
thrclark <thrclark@users.noreply.github.com>
2020-02-25 11:10:18 -0500

    setup

---
+++ php/includes/styles-admin.php
@@ -0,0 +1,8 @@
+<link href="../css/admin.css" rel="stylesheet" type="text/css">
+<style>
+:root {
+ --body-bg-color: #EAEAEA;
+ --logo-image-url: url("http://www.indiana.edu/~espd/int/img/trident-large.png");
+}
+
+</style>
